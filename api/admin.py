5cc018e00f3490a0e0705ee0a12d2ad8604b85f4
Michael Gabbard <zanvoysan@gmail.com>
2020-06-18 11:10:32 -0400

    bootstrap command works. shoe api broken

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from api.models import Shoe, ShoeColor, ShoeType, Manufacturer
 # Register your models here.
+admin.site.register(Shoe)
+admin.site.register(ShoeColor)
+admin.site.register(ShoeType)
+admin.site.register(Manufacturer)
